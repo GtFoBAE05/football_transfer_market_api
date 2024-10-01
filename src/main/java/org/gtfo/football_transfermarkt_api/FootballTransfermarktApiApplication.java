803f565978ec405dd1a86561f1f79896464d980a
GtFoBAE05 <y.imannuel@gmail.com>
2024-10-01 20:32:26 +0700

    initial commit

---
+++ src/main/java/org/gtfo/football_transfermarkt_api/FootballTransfermarktApiApplication.java
@@ -0,0 +1,13 @@
+package org.gtfo.football_transfermarkt_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballTransfermarktApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballTransfermarktApiApplication.class, args);
+    }
+
+}
